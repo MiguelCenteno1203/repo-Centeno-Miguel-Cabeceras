b4cdb894b99b3faf8764acc9863f7c437e40beb5
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:26:20 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Ingrese 1er número: ");
+    scanf("%d", &a);
+    printf("Ingrese 2do número: ");
+    scanf("%d", &b);
+}
