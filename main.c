1ea25822c54a9983af64c2e1683a667730e19f41
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 12:07:07 -0500

    third commit

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "sumar.h"
+#include "restar.h"
+#include "multiplicar.h"
 int main(int argc, char const *argv[])
 {
     int a,b;
@@ -8,5 +10,7 @@ int main(int argc, char const *argv[])
     printf("Ingrese 2do número: ");
     scanf("%d", &b);
     printf("la suma es: %d",sumar(a,b));
+    printf("la resta es: %d",restar(a,b));
+    printf("la multiplicación es: %d",multiplicar(a,b));
     return 0;
 }
